c84d42193dac52f13ddbfbf07df3ecb25b19e454
lusorich <dreamonautre@gmail.com>
2021-09-18 21:10:57 +0500

    step 4

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export const helloUser = () => {
   const name = readlineSync.question('Your answer: ');
-  console.log(`Hello, ${name}`!);
+  console.log(`Hello, ${name} !`);
 };
